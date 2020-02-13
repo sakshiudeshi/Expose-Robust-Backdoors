902cf21b0a95ac2bc968eb61ccb66f34d8003d55
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-13 05:25:46 +0000

    MNIST Clean

---
+++ MNIST/MNIST-Clean/user_constants.py
@@ -0,0 +1,11 @@
+# Path to dataset 
+DATA_PATH_DICT = {
+    'FashionMNIST': '/tmp/',
+    'MNIST': '/tmp/',
+    'CIFAR': '/tmp/',
+    'RestrictedImageNet': '/scratch/engstrom_scratch/imagenet',
+    'ImageNet': '/scratch/engstrom_scratch/imagenet',
+    'H2Z': '/scratch/datasets/A2B/horse2zebra',
+    'A2O': '/scratch/datasets/A2B/apple2orange',
+    'S2W': '/scratch/datasets/A2B/summer2winter_yosemite'
+}
